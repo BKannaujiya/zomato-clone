aac6b648eff0c36fb744bae0bb35c7d20df19a43
BASHISHTHA KANNAUJIYA <90121632+BKannaujiya@users.noreply.github.com>
2023-05-31 19:53:15 +0530

    Add files via upload

---
+++ precache-manifest.ed461ab911a0de01469171e6a46e142b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99531cb17cf5fda984d0613c1b26d32e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c053cc43e4cec5a2e838",
+    "url": "/static/css/main.cb86be69.chunk.css"
+  },
+  {
+    "revision": "b94b778fbb9ea1a5c128",
+    "url": "/static/js/2.ba3d7157.chunk.js"
+  },
+  {
+    "revision": "c053cc43e4cec5a2e838",
+    "url": "/static/js/main.17e499fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
